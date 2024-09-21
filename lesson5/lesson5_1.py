60e19a35a0dca02659b8ae1fe333f493947accf2
Sean <f109152130@nkust.edu.tw>
2024-09-21 16:09:40 +0800

    0921第三次更新

--- lesson5/lesson5_1.py
+++ lesson5/lesson5_1.py
@@ -1,14 +1,16 @@
 from machine import Timer
+from machine import Pin
 
 #tim = Timer(period=5000, mode=Timer.ONE_SHOT, callback=lambda t:print(1))
 #tim.init(period=1000, mode=Timer.PERIODIC, callback=lambda t:print(2))
 count = 0
-
+green_led = Pin("LED", Pin.OUT)
 def mycallback(t:Timer):
     global count
     count = count + 1
-    print(f"目前mycallback被執行:{count}次")
+    #print(f"目前mycallback被執行:{count}次")
+    green_led.toggle()#toggle狀態反向
     if count >= 10:
         t.deinit()
         
-led_timer = Timer(period=1000, mode=Timer.PERIODIC, callback=mycallback)
+green_led_timer = Timer(period=1000, mode=Timer.PERIODIC, callback=mycallback)
