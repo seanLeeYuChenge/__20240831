9d97567f0624da3e9f6aa32c56c73a4df12a732b
李育丞 <135806422+seanLeeYuChenge@users.noreply.github.com>
2024-10-26 12:03:16 +0800

    1026第二次更新

---
+++ lesson10/config.py
@@ -0,0 +1,6 @@
+BLYNK_TEMPLATE_ID   = "TMPL65yUPTdbX"
+BLYNK_TEMPLATE_NAME = "pico"
+BLYNK_AUTH_TOKEN    = "JvTfOxUEweeT3LkJDv5REY-h5KVpOtuv"
+
+# Change the default Blynk server. Applicable for users with a white label plan.
+BLYNK_MQTT_BROKER   = "blynk.cloud"
